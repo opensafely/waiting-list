bcef397334b05ee71f86b394a129494ec41eabcd
Andrea Schaffer <37759997+alschaffer@users.noreply.github.com>
2023-10-23 17:18:13 +0100

    Clockstops dataset def and measures

---
+++ analysis/dataset_definition_clockstops.py
@@ -0,0 +1,266 @@
+################################################################################
+# This script defines a function to extract
+# relevant variables for people with a completed
+# waiting list from May 2021 - May 2022 regardless of treatment type/specialty
+################################################################################
+
+
+from ehrql import create_dataset, case, when, days, years, minimum_of
+from ehrql.tables.beta.tpp import (
+    patients, 
+    medications, 
+    addresses,
+    practice_registrations,
+    clinical_events,
+    wl_clockstops)
+
+import codelists
+
+
+dataset = create_dataset()
+
+
+#### Waiting list variables ####
+
+# WL data - exclude rows with missing dates/dates outside study period/end date before start date
+clockstops = wl_clockstops.where(
+        wl_clockstops.referral_to_treatment_period_end_date.is_on_or_between("2021-05-01", "2022-05-01")
+        & wl_clockstops.referral_to_treatment_period_start_date.is_on_or_before(wl_clockstops.referral_to_treatment_period_end_date)
+        & wl_clockstops.week_ending_date.is_on_or_between("2021-05-01", "2022-05-01")
+        & wl_clockstops.waiting_list_type.is_in(["IRTT","ORTT","PTLO","PTLI","PLTI","RTTO","RTTI","PTL0","PTL1"])
+    )
+
+# Number of RTT pathways per person
+dataset.count_rtt_pathways = clockstops.count_for_patient()
+
+# Latest waiting list
+#   Sort by IDs and start date to identify unique RTT pathways
+last_clockstops = clockstops.sort_by(
+        clockstops.pseudo_patient_pathway_identifier,
+        clockstops.pseudo_organisation_code_patient_pathway_identifier_issuer,
+        clockstops.pseudo_referral_identifier,
+        clockstops.referral_to_treatment_period_start_date
+    ).last_for_patient()
+
+# RTT waiting list start date and end date
+dataset.rtt_start_date = last_clockstops.referral_to_treatment_period_start_date
+dataset.rtt_end_date = last_clockstops.referral_to_treatment_period_end_date
+dataset.wait_time = (dataset.rtt_end_date - dataset.rtt_start_date).days
+
+# Other relevant columns
+dataset.treatment_function = last_clockstops.activity_treatment_function_code
+dataset.waiting_list_type = last_clockstops.waiting_list_type
+dataset.priority_type = last_clockstops.priority_type_code
+
+
+#### Censoring dates ####
+
+# Registered 6 months before WL start
+registrations = practice_registrations.where(
+        practice_registrations.start_date.is_on_or_before(dataset.rtt_start_date - days(182))
+        & (practice_registrations.end_date.is_after(dataset.rtt_start_date) | practice_registrations.end_date.is_null())
+    )
+
+dataset.reg_end_date = registrations.sort_by(registrations.end_date).last_for_patient().end_date
+dataset.dod = patients.date_of_death
+dataset.end_date = minimum_of(dataset.reg_end_date, dataset.dod, dataset.rtt_end_date + days(182))
+
+# Flag if censored before WL end date
+dataset.censor_before_rtt_end = dataset.end_date < dataset.rtt_end_date
+
+# Flag if censored before study end date (RTT end + 6 months)
+dataset.censor_before_study_end = dataset.end_date < dataset.rtt_end_date + days(182)
+
+
+#### Medicines data ####
+
+# Number of prescriptions during waiting list (this time period is variable, will account for this later)
+def count_med_wait(codelist):
+    return medications.where(
+            medications.dmd_code.is_in(codelist)
+            & medications.date.is_on_or_between(dataset.rtt_start_date, minimum_of(dataset.end_date, dataset.rtt_end_date))
+        ).count_for_patient()
+
+# Number of prescriptions before waiting list
+def count_med_pre(codelist):
+    return medications.where(
+            medications.dmd_code.is_in(codelist)
+            & medications.date.is_on_or_between(dataset.rtt_start_date - days(182), dataset.rtt_start_date - days(1))
+        ).count_for_patient()
+
+# Number of prescriptions after waiting list
+def count_med_post(codelist):
+    return medications.where(
+            medications.dmd_code.is_in(codelist)
+            & medications.date.is_on_or_between(dataset.rtt_end_date + days(1), minimum_of(dataset.rtt_end_date + days(182), dataset.end_date))
+            & (dataset.end_date > dataset.rtt_end_date)
+        ).count_for_patient()
+
+
+# Any opioid
+dataset.opioid_wait_count = count_med_wait(codelists.opioid_codes)
+dataset.opioid_pre_count = count_med_pre(codelists.opioid_codes)
+dataset.opioid_post_count = count_med_post(codelists.opioid_codes)
+
+# High dose/long-acting opioids
+dataset.hi_opioid_wait_count = count_med_wait(codelists.hi_opioid_codes)
+dataset.hi_opioid_pre_count = count_med_pre(codelists.hi_opioid_codes)
+dataset.hi_opioid_post_count = count_med_post(codelists.hi_opioid_codes)
+
+# Gabapentinoids
+dataset.gaba_wait_count = count_med_wait(codelists.gabapentinoid_codes)
+dataset.gaba_pre_count = count_med_pre(codelists.gabapentinoid_codes)
+dataset.gaba_post_count = count_med_post(codelists.gabapentinoid_codes)
+
+# Antidepressant
+dataset.ad_wait_count = count_med_wait(codelists.antidepressant_codes)
+dataset.ad_pre_count = count_med_pre(codelists.antidepressant_codes)
+dataset.ad_post_count = count_med_post(codelists.antidepressant_codes)
+
+# NSAID
+dataset.nsaid_wait_count = count_med_wait(codelists.nsaid_codes)
+dataset.nsaid_pre_count = count_med_pre(codelists.nsaid_codes)
+dataset.nsaiad_post_count = count_med_post(codelists.nsaid_codes)
+
+
+#### Demographics ####
+
+dataset.age = patients.age_on(dataset.rtt_start_date)
+dataset.age_group = case(
+        when(dataset.age < 40).then("18-39"),
+        when(dataset.age < 50).then("40-49"),
+        when(dataset.age < 60).then("50-59"),
+        when(dataset.age < 70).then("60-69"),
+        when(dataset.age < 80).then("70-79"),
+        when(dataset.age >= 80).then("80+"),
+        default="Missing",
+)
+dataset.sex = patients.sex
+
+# IMD decile
+imd = addresses.for_patient_on(dataset.rtt_start_date).imd_rounded
+dataset.imd10 = case(
+        when((imd >= 0) & (imd < int(32844 * 1 / 10))).then("1 (most deprived)"),
+        when(imd < int(32844 * 2 / 10)).then("2"),
+        when(imd < int(32844 * 3 / 10)).then("3"),
+        when(imd < int(32844 * 4 / 10)).then("4"),
+        when(imd < int(32844 * 5 / 10)).then("5"),
+        when(imd < int(32844 * 6 / 10)).then("6"),
+        when(imd < int(32844 * 7 / 10)).then("7"),
+        when(imd < int(32844 * 8 / 10)).then("8"),
+        when(imd < int(32844 * 9 / 10)).then("9"),
+        when(imd >= int(32844 * 9 / 10)).then("10 (least deprived)"),
+        default="Unknown"
+)
+
+# Ethnicity 6 categories
+ethnicity6 = clinical_events.where(
+        clinical_events.snomedct_code.is_in(codelists.ethnicity_codes_6)
+    ).where(
+        clinical_events.date.is_on_or_before(dataset.rtt_start_date)
+    ).sort_by(
+        clinical_events.date
+    ).last_for_patient().snomedct_code.to_category(codelists.ethnicity_codes_6)
+
+dataset.ethnicity6 = case(
+    when(ethnicity6 == "1").then("White"),
+    when(ethnicity6 == "2").then("Mixed"),
+    when(ethnicity6 == "3").then("South Asian"),
+    when(ethnicity6 == "4").then("Black"),
+    when(ethnicity6 == "5").then("Other"),
+    when(ethnicity6 == "6").then("Not stated"),
+    default="Unknown"
+)
+
+# Ethnicity 16 categories
+ethnicity16 = clinical_events.where(
+        clinical_events.snomedct_code.is_in(codelists.ethnicity_codes_16)
+    ).where(
+        clinical_events.date.is_on_or_before(dataset.rtt_start_date)
+    ).sort_by(
+        clinical_events.date
+    ).last_for_patient().snomedct_code.to_category(codelists.ethnicity_codes_16)
+
+dataset.ethnicity16 = case(
+    when(ethnicity16 == "1").then("White - British"),
+    when(ethnicity16 == "2").then("White - Irish"),
+    when(ethnicity16 == "3").then("White - Other"),
+    when(ethnicity16 == "4").then("Mixed - White/Black Caribbean"),
+    when(ethnicity16 == "5").then("Mixed - White/Black African"),
+    when(ethnicity16 == "6").then("Mixed - White/Asian"),
+    when(ethnicity16 == "7").then("Mixed - Other"),
+    when(ethnicity16 == "8").then("Asian or Asian British - Indian"),
+    when(ethnicity16 == "9").then("Asian or Asian British - Pakistani"),
+    when(ethnicity16 == "10").then("Asian or Asian British - Bangladeshi"),
+    when(ethnicity16 == "11").then("Asian or Asian British - Other"),
+    when(ethnicity16 == "12").then("Black - Caribbean"),    
+    when(ethnicity16 == "13").then("Black - African"),
+    when(ethnicity16 == "14").then("Black - Other"),
+    when(ethnicity16 == "15").then("Other - Chinese"),
+    when(ethnicity16 == "16").then("Other - Other"),
+    default="Unknown"
+)
+
+dataset.region = practice_registrations.for_patient_on(dataset.rtt_start_date).practice_nuts1_region_name
+
+
+#### Clinical characteristics ####
+
+# Cancer diagnosis in past 5 years 
+dataset.cancer = clinical_events.where(
+        clinical_events.snomedct_code.is_in(codelists.cancer_codes)
+    ).where(
+        clinical_events.date.is_on_or_between(dataset.rtt_start_date - years(5), dataset.rtt_start_date)
+    ).exists_for_patient()
+
+# All clinical events - past 6 months
+clin_events_6mo = clinical_events.where(
+        clinical_events.date.is_on_or_between(dataset.rtt_start_date, dataset.rtt_start_date - days(182))
+    )
+
+# Comorbidities in past 6 mos
+dataset.diabetes = clin_events_6mo.where(
+        clin_events_6mo.ctv3_code.is_in(codelists.diabetes_codes)
+    ).exists_for_patient()
+
+dataset.cardiac = clin_events_6mo.where(
+        clin_events_6mo.ctv3_code.is_in(codelists.cardiac_codes)
+    ).exists_for_patient()
+
+dataset.copd = clin_events_6mo.where(
+        clin_events_6mo.ctv3_code.is_in(codelists.copd_codes)
+    ).exists_for_patient()
+
+dataset.liver = clin_events_6mo.where(
+        clin_events_6mo.ctv3_code.is_in(codelists.liver_codes)
+    ).exists_for_patient()
+
+dataset.ckd = clin_events_6mo.where(
+        clin_events_6mo.snomedct_code.is_in(codelists.ckd_codes)
+    ).exists_for_patient()
+
+dataset.osteoarthritis = clin_events_6mo.where(
+        clin_events_6mo.ctv3_code.is_in(codelists.osteo_codes)
+    ).exists_for_patient()
+
+dataset.depression = clin_events_6mo.where(
+        clin_events_6mo.ctv3_code.is_in(codelists.depression_codes)
+    ).exists_for_patient()
+
+# dataset.anxiety = clin_events_6mo.where(
+#         clin_events_6mo.ctv3_code.is_in(codelists.anxiety_codes)
+#     ).exists_for_patient()
+
+### TO ADD MORE? ###
+
+
+#### DEFINE POPULATION ####
+
+dataset.define_population(
+    (dataset.age >= 18) 
+    & (dataset.age < 110)
+    & dataset.sex.is_in(["male","female"])
+    & dataset.end_date.is_after(dataset.rtt_start_date)
+    & registrations.exists_for_patient()
+    & last_clockstops.exists_for_patient()
+)
---
+++ analysis/measures_clockstops.py
@@ -0,0 +1,243 @@
+###########################################################
+# This script creates weekly opioid prescribing rates
+#   in the 6 months pre-WL, during WL and 6 months post-WL
+#   for people with a completed RTT pathway
+#   for orthopaedic surgery
+###########################################################
+
+from ehrql import INTERVAL, Measures, weeks, days, minimum_of, case, when
+from ehrql.tables.beta.tpp import (
+    patients, 
+    practice_registrations,
+    medications,
+    addresses,
+    wl_clockstops)
+
+import codelists
+
+##########
+
+# WL data - one wait per person
+ortho_clockstops = wl_clockstops.where(
+        wl_clockstops.referral_to_treatment_period_end_date.is_on_or_between("2021-05-01", "2022-05-01")
+        & wl_clockstops.referral_to_treatment_period_start_date.is_on_or_before(wl_clockstops.referral_to_treatment_period_end_date)
+        & wl_clockstops.week_ending_date.is_on_or_between("2021-05-01", "2022-05-01")
+        & wl_clockstops.waiting_list_type.is_in(["IRTT","ORTT","PTLO","PTLI","PLTI","RTTO","RTTI","PTL0","PTL1"])
+        & wl_clockstops.activity_treatment_function_code.is_in(["110"])
+    ).sort_by(
+        wl_clockstops.pseudo_patient_pathway_identifier,
+        wl_clockstops.pseudo_organisation_code_patient_pathway_identifier_issuer,
+        wl_clockstops.pseudo_referral_identifier,
+        wl_clockstops.referral_to_treatment_period_start_date
+    ).last_for_patient()
+
+# # Restrict to orthopaedic surgery 
+# ortho_clockstops = last_clockstops.where(
+#         last_clockstops.activity_treatment_function_code.is_in(["110"])
+#     )
+
+
+# RTT waiting list start date and end date
+rtt_start_date = ortho_clockstops.referral_to_treatment_period_start_date
+rtt_end_date = ortho_clockstops.referral_to_treatment_period_end_date
+
+# Set artificial start date for running Measures
+tmp_rtt_start_date = "2000-01-01"
+tmp_rtt_end_date = "2000-01-01"
+
+# All opioid prescriptions 
+all_opioid_rx = medications.where(
+                ortho_clockstops.exists_for_patient()
+                & medications.dmd_code.is_in(codelists.opioid_codes)
+                & medications.date.is_on_or_after(rtt_start_date - days(182))
+            )
+
+# Standardise Rx dates relative to RTT start date for prescribing during WL 
+all_opioid_rx.tmp_wait_date = tmp_rtt_start_date + days((all_opioid_rx.date - rtt_start_date).days)
+
+# Standardise Rx dates relative to RTT end date for post-WL prescribing
+all_opioid_rx.tmp_post_date = tmp_rtt_end_date + days((all_opioid_rx.date - rtt_end_date).days)
+
+# Standardise Rx dates relative to RTT start date for pre-WL prescribing (note: dates count backwards from start date)
+all_opioid_rx.tmp_pre_date = tmp_rtt_start_date - days((all_opioid_rx.date - rtt_start_date).days)
+
+
+### Grouping/stratification variables (List TBD) ###
+
+prior_opioid_rx = all_opioid_rx.where(
+        all_opioid_rx.date.is_between_but_not_on(rtt_start_date - days(183), rtt_start_date)
+    ).exists_for_patient()
+
+
+
+### Opioid variables ####
+
+### All opioids
+
+# Num opioid Rx during waiting list
+count_opioid_wait = all_opioid_rx.where(
+                all_opioid_rx.tmp_wait_date.is_on_or_between(INTERVAL.start_date, INTERVAL.end_date)
+            ).count_for_patient()
+
+# Num opioid Rx post waiting list (up to 6 months)
+count_opioid_post = all_opioid_rx.where(
+                all_opioid_rx.tmp_post_date.is_on_or_between(INTERVAL.start_date, INTERVAL.end_date)
+            ).count_for_patient()
+
+# Num opioid Rx pre waiting list (up to 6 months)
+count_opioid_pre = all_opioid_rx.where(
+                all_opioid_rx.tmp_pre_date.is_on_or_between(INTERVAL.start_date, INTERVAL.end_date)
+            ).count_for_patient()
+
+
+### High dose opioids
+
+# Num opioid Rx during waiting list
+count_hi_opioid_wait = all_opioid_rx.where(
+                all_opioid_rx.dmd_code.is_in(codelists.hi_opioid_codes)
+                & all_opioid_rx.tmp_wait_date.is_on_or_between(INTERVAL.start_date, INTERVAL.end_date)
+            ).count_for_patient()
+
+# Num opioid Rx post waiting list (up to 6 months)
+count_hi_opioid_post = all_opioid_rx.where(
+                all_opioid_rx.dmd_code.is_in(codelists.hi_opioid_codes)
+                & all_opioid_rx.tmp_post_date.is_on_or_between(INTERVAL.start_date, INTERVAL.end_date)
+            ).count_for_patient()
+
+# Num opioid Rx pre waiting list (up to 6 months)
+count_hi_opioid_pre = all_opioid_rx.where(
+                all_opioid_rx.dmd_code.is_in(codelists.hi_opioid_codes)
+                & all_opioid_rx.tmp_pre_date.is_on_or_between(INTERVAL.start_date, INTERVAL.end_date)
+            ).count_for_patient()
+
+
+## Censoring date
+registrations = practice_registrations.where(
+        practice_registrations.start_date.is_on_or_before(rtt_start_date - days(182))
+        & (practice_registrations.end_date.is_after(rtt_start_date) | practice_registrations.end_date.is_null())
+    )
+
+reg_end_date = registrations.sort_by(registrations.end_date).last_for_patient().end_date
+end_date = minimum_of(reg_end_date, patients.date_of_death, rtt_end_date + days(182))
+
+
+######
+
+
+measures = Measures()
+
+# Denominator = everyone on the waiting list with non-missing age/sex, 
+#   and whose end date (i.e. death or deregistration) is after the end of the interval
+denominator = (        
+        (patients.age_on(rtt_start_date) >= 18) 
+        & (patients.age_on(rtt_start_date) < 110)
+        & (patients.sex.is_in(["male","female"]))
+        & ortho_clockstops.exists_for_patient()
+        & registrations.exists_for_patient()
+        & (end_date > INTERVAL.end_date)
+    )
+
+## All opioids
+# Prescribing during WL
+measures.define_measure(
+    name="count_opioid_wait",
+    numerator=count_opioid_wait,
+    denominator=denominator,
+    intervals=weeks(52).starting_on("2000-01-01")
+    )
+
+# Prescribing post WL
+measures.define_measure(
+    name="count_opioid_post",
+    numerator=count_opioid_post,
+    denominator=denominator,
+    intervals=weeks(26).starting_on("2000-01-01")
+    )
+
+# Prescribing pre WL
+measures.define_measure(
+    name="count_opioid_pre",
+    numerator=count_opioid_pre,
+    denominator=denominator,
+    intervals=weeks(26).starting_on("2000-01-01")
+    )
+
+# Prescribing during WL - stratified by prior opioid Rx
+measures.define_measure(
+    name="count_opioid_wait_prior",
+    numerator=count_opioid_wait,
+    denominator=denominator,
+    intervals=weeks(52).starting_on("2000-01-01"),
+    group_by={"prior_opioid_rx": prior_opioid_rx}
+    )
+
+# Prescribing post WL - stratified by prior opioid Rx
+measures.define_measure(
+    name="count_opioid_post_prior",
+    numerator=count_opioid_post,
+    denominator=denominator,
+    intervals=weeks(26).starting_on("2000-01-01"),
+    group_by={"prior_opioid_rx": prior_opioid_rx}
+    )
+
+# Prescribing pre WL - stratified by prior opioid Rx
+measures.define_measure(
+    name="count_opioid_pre_prior",
+    numerator=count_opioid_pre,
+    denominator=denominator,
+    intervals=weeks(26).starting_on("2000-01-01"),
+    group_by={"prior_opioid_rx": prior_opioid_rx}
+    )
+
+## High dose opioids
+
+# Prescribing during WL
+measures.define_measure(
+    name="count_hi_opioid_wait",
+    numerator=count_hi_opioid_wait,
+    denominator=denominator,
+    intervals=weeks(52).starting_on("2000-01-01")
+    )
+
+# Prescribing post WL
+measures.define_measure(
+    name="count_hi_opioid_post",
+    numerator=count_hi_opioid_post,
+    denominator=denominator,
+    intervals=weeks(26).starting_on("2000-01-01")
+    )
+
+# Prescribing pre WL
+measures.define_measure(
+    name="count_hi_opioid_pre",
+    numerator=count_hi_opioid_pre,
+    denominator=denominator,
+    intervals=weeks(26).starting_on("2000-01-01")
+    )
+
+# Prescribing during WL - stratified by prior opioid Rx
+measures.define_measure(
+    name="count_hi_opioid_wait_prior",
+    numerator=count_hi_opioid_wait,
+    denominator=denominator,
+    intervals=weeks(52).starting_on("2000-01-01"),
+    group_by={"prior_opioid_rx": prior_opioid_rx}
+    )
+
+# Prescribing post WL - stratified by prior opioid Rx
+measures.define_measure(
+    name="count_hi_opioid_post_prior",
+    numerator=count_hi_opioid_post,
+    denominator=denominator,
+    intervals=weeks(26).starting_on("2000-01-01"),
+    group_by={"prior_opioid_rx": prior_opioid_rx}
+    )
+
+# Prescribing pre WL - stratified by prior opioid Rx
+measures.define_measure(
+    name="count_hi_opioid_pre_prior",
+    numerator=count_hi_opioid_pre,
+    denominator=denominator,
+    intervals=weeks(26).starting_on("2000-01-01"),
+    group_by={"prior_opioid_rx": prior_opioid_rx}
+    )
