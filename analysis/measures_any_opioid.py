a69a75a3ad338dab4062981c2224ba874ab875e5
Andrea Schaffer <37759997+alschaffer@users.noreply.github.com>
2023-11-01 14:27:35 +0000

    Fix measures code

--- analysis/measures_any_opioid.py
+++ analysis/measures_any_opioid.py
@@ -5,7 +5,7 @@
 #   for orthopaedic surgery only
 ###########################################################
 
-from ehrql import INTERVAL, Measures, weeks, days, minimum_of, case, when, years
+from ehrql import INTERVAL, Measures, weeks, days, minimum_of, years
 from ehrql.tables.beta.tpp import (
     patients, 
     practice_registrations,
@@ -41,6 +41,7 @@ rtt_end_date = last_clockstops.referral_to_treatment_period_end_date
 # Set artificial start/end date for running Measures
 tmp_rtt_start_date = "2000-01-01"
 tmp_rtt_end_date = "2000-01-01"
+tmp_study_start_date = "2000-01-01"
 
 # All opioid prescriptions 
 all_opioid_rx = medications.where(
@@ -54,16 +55,14 @@ all_opioid_rx.tmp_wait_date = tmp_rtt_start_date + days((all_opioid_rx.date - rt
 # Standardise Rx dates relative to RTT end date for post-WL prescribing
 all_opioid_rx.tmp_post_date = tmp_rtt_end_date + days((all_opioid_rx.date - rtt_end_date).days)
 
-# Standardise Rx dates relative to RTT start date for pre-WL prescribing (note: dates count backwards from start date)
-all_opioid_rx.tmp_pre_date = tmp_rtt_start_date - days((all_opioid_rx.date - rtt_start_date).days)
+# Standardise Rx dates relative to RTT start date for pre-WL prescribing 
+all_opioid_rx.tmp_pre_date = tmp_study_start_date - days((all_opioid_rx.date - (rtt_start_date - days(182))).days)
 
 
 ### Grouping/stratification variables (Final list TBD) ###
-
 prior_opioid_rx = all_opioid_rx.where(
         all_opioid_rx.date.is_on_or_between(rtt_start_date - days(182), rtt_start_date - days(1))
     ).exists_for_patient()
-
 
 
 ### Opioid variables ####
@@ -95,6 +94,9 @@ registrations = practice_registrations.where(
 reg_end_date = registrations.sort_by(registrations.end_date).last_for_patient().end_date
 end_date = minimum_of(reg_end_date, patients.date_of_death, rtt_end_date + days(182))
 
+tmp_end_date_rtt_start = tmp_rtt_start_date + days((end_date - rtt_start_date).days)
+tmp_end_date_rtt_end = tmp_rtt_end_date + days((end_date - rtt_end_date).days)
+
 
 # Cancer diagnosis in past 5 years 
 cancer = clinical_events.where(
@@ -117,7 +119,6 @@ denominator = (
         & (patients.sex.is_in(["male","female"]))
         & last_clockstops.exists_for_patient()
         & registrations.exists_for_patient()
-        & (end_date > INTERVAL.end_date)
         & ortho_surgery
         & ~cancer
     )
@@ -127,7 +128,7 @@ denominator = (
 measures.define_measure(
     name="count_opioid_wait",
     numerator=count_opioid_wait,
-    denominator=denominator,
+    denominator=denominator & (tmp_end_date_rtt_start > INTERVAL.end_date),
     intervals=weeks(52).starting_on("2000-01-01")
     )
 
@@ -135,7 +136,7 @@ measures.define_measure(
 measures.define_measure(
     name="count_opioid_post",
     numerator=count_opioid_post,
-    denominator=denominator,
+    denominator=denominator & (tmp_end_date_rtt_end > INTERVAL.end_date),
     intervals=weeks(26).starting_on("2000-01-01")
     )
 
@@ -144,32 +145,16 @@ measures.define_measure(
     name="count_opioid_pre",
     numerator=count_opioid_pre,
     denominator=denominator,
-    intervals=weeks(26).starting_on("2000-01-01")
+    intervals=weeks(26).starting_on("2000-01-02")
     )
 
 # Prescribing during WL - stratified by prior opioid Rx
 measures.define_measure(
     name="count_opioid_wait_prior",
     numerator=count_opioid_wait,
-    denominator=denominator,
+    denominator=denominator & (tmp_end_date_rtt_start > INTERVAL.end_date),
     intervals=weeks(52).starting_on("2000-01-01"),
     group_by={"prior_opioid_rx": prior_opioid_rx}
     )
 
-# Prescribing post WL - stratified by prior opioid Rx
-measures.define_measure(
-    name="count_opioid_post_prior",
-    numerator=count_opioid_post,
-    denominator=denominator,
-    intervals=weeks(26).starting_on("2000-01-01"),
-    group_by={"prior_opioid_rx": prior_opioid_rx}
-    )
 
-# Prescribing pre WL - stratified by prior opioid Rx
-measures.define_measure(
-    name="count_opioid_pre_prior",
-    numerator=count_opioid_pre,
-    denominator=denominator,
-    intervals=weeks(26).starting_on("2000-01-01"),
-    group_by={"prior_opioid_rx": prior_opioid_rx}
-    )
