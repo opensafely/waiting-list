d543fb6f736a030a5b613ff8a4ea05cd9058367a
Andrea Schaffer <37759997+alschaffer@users.noreply.github.com>
2023-11-09 13:01:48 +0000

    Update ortho codelist

--- analysis/measures_checks.py
+++ analysis/measures_checks.py
@@ -22,18 +22,18 @@ count_rtt_end_date = wl_clockstops.where(
 count_rtt_end_date_ortho = wl_clockstops.where(
         wl_clockstops.referral_to_treatment_period_end_date.is_on_or_between(INTERVAL.start_date, INTERVAL.end_date)
         #& wl_clockstops.waiting_list_type.is_in(["IRTT","PTLI","PLTI","RTTI","PTL1","ORTT","PTLO","RTTO","PTL0"])
-        & wl_clockstops.activity_treatment_function_code.is_in(["110","111"])
+        & wl_clockstops.activity_treatment_function_code.is_in(["110","111","108","115"])
     ).pseudo_referral_identifier.count_distinct_for_patient()
 
 count_admitted = wl_clockstops.where(
         wl_clockstops.referral_to_treatment_period_end_date.is_on_or_between(INTERVAL.start_date, INTERVAL.end_date)
-        & wl_clockstops.waiting_list_type.is_in(["IRTT","PTLI","PLTI","RTTI","PTL1"])
+        & wl_clockstops.waiting_list_type.is_in(["110","111","108","115"])
     ).pseudo_referral_identifier.count_distinct_for_patient()
 
 count_admitted_ortho = wl_clockstops.where(
         wl_clockstops.referral_to_treatment_period_end_date.is_on_or_between(INTERVAL.start_date, INTERVAL.end_date)
         & wl_clockstops.waiting_list_type.is_in(["IRTT","PTLI","PLTI","RTTI","PTL1"])
-        & wl_clockstops.activity_treatment_function_code.is_in(["110","111"])
+        & wl_clockstops.activity_treatment_function_code.is_in(["110","111","108","115"])
     ).pseudo_referral_identifier.count_distinct_for_patient()
 
 count_not_admitted = wl_clockstops.where(
@@ -44,7 +44,7 @@ count_not_admitted = wl_clockstops.where(
 count_not_admitted_ortho = wl_clockstops.where(
         wl_clockstops.referral_to_treatment_period_end_date.is_on_or_between(INTERVAL.start_date, INTERVAL.end_date)
         & wl_clockstops.waiting_list_type.is_in(["ORTT","PTLO","RTTO","PTL0"])
-        & wl_clockstops.activity_treatment_function_code.is_in(["110","111"])
+        & wl_clockstops.activity_treatment_function_code.is_in(["110","111","108","115"])
     ).pseudo_referral_identifier.count_distinct_for_patient()
 
 
