fcd7e517193946944774cb6c42535fb710677152
Andrea Schaffer <37759997+alschaffer@users.noreply.github.com>
2024-03-05 20:08:08 +0000

    Typos

--- analysis/dataset_definition_full.py
+++ analysis/dataset_definition_full.py
@@ -44,34 +44,34 @@ last_clockstops = clockstops.sort_by(
         clockstops.pseudo_organisation_code_patient_pathway_identifier_issuer
     ).last_for_patient()
 
-rtt_start_date = last_clockstops.referral_to_treatment_period_start_date
-rtt_end_date = last_clockstops.referral_to_treatment_period_end_date
+dataset.rtt_start_date = last_clockstops.referral_to_treatment_period_start_date
+dataset.rtt_end_date = last_clockstops.referral_to_treatment_period_end_date
 
 
 #### Censoring dates ####
 
 # Registered 6 months before WL start
 registrations = practice_registrations.spanning(
-        rtt_start_date - days(182), rtt_start_date
+        dataset.rtt_start_date - days(182), dataset.rtt_start_date
     ).sort_by(
         practice_registrations.end_date
     ).last_for_patient()
 
 reg_end_date = registrations.end_date
 dod = patients.date_of_death
-dataset.end_date = minimum_of(reg_end_date, dod, rtt_end_date + days(182))
+dataset.end_date = minimum_of(reg_end_date, dod, dataset.rtt_end_date + days(182))
 
 
 #### Demographics ####
 
-dataset.age = patients.age_on(rtt_start_date)
+dataset.age = patients.age_on(dataset.rtt_start_date)
 dataset.sex = patients.sex
 
 
 #### DEFINE POPULATION ####
 
 dataset.define_population(
-    dataset.end_date.is_after(rtt_start_date)
+    dataset.end_date.is_after(dataset.rtt_start_date)
     & registrations.exists_for_patient()
     & last_clockstops.exists_for_patient()
 )
